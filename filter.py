c145ee133a9434cc32f32fbd2dec4b1eade2c9bc
Caleb <caleb@shoponomik.com>
2021-10-16 22:03:53 -0700

    crawling finished

--- filter.py
+++ filter.py
@@ -2,17 +2,26 @@ import json
 import re
 
 f = open('pull_requests_copy.json')
+f2 = open('issues_copy.json')
 
-data = json.load(f) 
+pull_requests = json.load(f) 
+issues = json.load(f2)
 output_file = open('pull_requests_filtered.json', 'w') 
-
-for pr in data:
-    if pr['linked_issue']:
-        pr['linked_issue'][1] = pr['linked_issue'][1].replace('\n', '')
-        pr['linked_issue'][0] = ''
+output_file2 = open('issues_filtered.json', 'w') 
 
 f.close()
+f2.close()
 
-json.dump(data, output_file)
+for pr in pull_requests:
+    if pr['linked_issue']:
+        pr['linked_issue'][1] = pr['linked_issue'][1].replace('\n', '').replace(' ', '')
+        pr['linked_issue'][0] = ''
+
+for issue in issues:
+    issue['title'] = issue['title'].replace(' ', '')
+
+json.dump(pull_requests, output_file)
+json.dump(issues, output_file2)
 
 output_file.close()
+output_file2.close()
