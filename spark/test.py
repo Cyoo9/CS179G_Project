792857b79264038e5bdd93d759ab823fe13f7dfa
Caleb <caleb@shoponomik.com>
2021-10-30 14:19:45 -0700

    limit of 25 fixed. some ids are undefined is why index goes out of range is fixed.

--- spark/test.py
+++ spark/test.py
@@ -1,23 +1,32 @@
 import pyspark
 from pyspark.sql import SQLContext
 
+
+
 sparkCont = pyspark.SparkContext('local[*]')
 sc = SQLContext(sparkCont)
 
 
-issues = sc.read.json('issues_filtered.json')
-issues.createOrReplaceTempView("title")
+issues = sc.read.option("multiline", "true").json("issues_filtered.json")
+issues.createOrReplaceTempView("issues_filtered_json")
+#sc.sql("SELECT * FROM issues_filtered_json").show()
 
-releases = sc.read.json('cs179g_crawler/releases.json')
-releases.createOrReplaceTempView("pull_request_ids")
+releases = sc.read.option("multiline", "true").json("./cs179g_crawler/releases.json")
+releases.createOrReplaceTempView("releases_json")
+df = sc.sql("SELECT * from releases_json")
+release_list = df.select('pull_request_ids').collect()
+#sc.sql("SELECT * FROM releases_json").show()
+'''for release in release_list:
+    for item in release:
+        for temp in item:
+            print(temp)''' #prints out the pull request release ID
 
-pull_requests = sc.read.json('cs179g_crawler/pull_requests.json')
-pull_requests.createOrReplaceTempView("linked_issue")
+pull_requests = sc.read.option("multiline", "true").json("pull_requests_filtered.json")
+pull_requests.createOrReplaceTempView("pull_requests_json")
+df = sc.sql("SELECT * from pull_requests_json")
+pull_list = df.select('id').collect()
 
-sc.sql("SELECT * FROM title").show()
-
-
-
-
-#match pull requests linked issue to issues_filtered title. match pull request id to ids in releases. 
-#if matched, compare releases date and issue date and take difference to find out how long it took for issue to be resolved. 
+print(len(pull_list))
+for pull in pull_list:
+    if(len(pull.id)):
+        print(pull.id[0])
